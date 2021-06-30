d80c1bd1331323d4a7f0b9edd7de89156fb73e4b
bpuhach <bpuhach@exadel.com>
2021-06-30 17:18:20 +0300

    add some istructions

--- src/app/core/components/info-modal/info-modal.component.ts
+++ src/app/core/components/info-modal/info-modal.component.ts
@@ -10,6 +10,8 @@ export class InfoModalComponent {
   public cliImage = '/assets/images/cli_version.png';
   public i1 = '/assets/images/instruction_1.png';
   public i2 = '/assets/images/instruction_2.png';
+  public i3 = '/assets/images/install2.jpg';
+  public i4 = '/assets/images/install1.jpg';
   constructor(
     public dialogRef: MatDialogRef<InfoModalComponent>
   ) {}
